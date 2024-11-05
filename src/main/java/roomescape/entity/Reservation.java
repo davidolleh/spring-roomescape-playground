58d780c559fede78cd2ef21f3fe1c8e858c66142
davidolleh <asherolleh@gmail.com>
2024-11-05 19:55:14 +0900

    <FIX> reservation id 자료형 Long으로 변경

--- src/main/java/roomescape/entity/Reservation.java
+++ src/main/java/roomescape/entity/Reservation.java
@@ -3,16 +3,25 @@ package roomescape.entity;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Reservation {
-    private int id;
+public class Reservation implements  Comparable<Long> {
+    private Long id;
     private Person person;
     private LocalDate date;
     private LocalTime time;
 
-    public Reservation(int id, Person person, LocalDate date, LocalTime time) {
-        this.id = id;
+    public Reservation(Person person, LocalDate date, LocalTime time) {
+        this.id = 0L;
         this.person = person;
         this.date = date;
         this.time = time;
+    }
+
+    @Override
+    public int compareTo(Long o) {
+        return this.id.compareTo(o);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
