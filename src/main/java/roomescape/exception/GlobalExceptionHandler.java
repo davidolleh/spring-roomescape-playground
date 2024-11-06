77a24b6cca4b275aea8fd80c0de1ff9e91e7a2aa
davidolleh <asherolleh@gmail.com>
2024-11-06 16:48:33 +0900

    <FEAT> 예외처리 추가

---
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package roomescape.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(EntityNotFoundException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
