2a3a59fbf55da4082bb58371f34b9150b2c4f62e
davidolleh <asherolleh@gmail.com>
2024-11-20 12:16:30 +0900

    <REFACTOR> 불필요한 주석 제거

--- src/main/java/roomescape/entity/Person.java
+++ src/main/java/roomescape/entity/Person.java
@@ -4,13 +4,8 @@ public class Person {
     private String name;
 
     public Person(String name) {
-//        this.id = id;
         this.name = name;
     }
-
-//    public Long getId() {
-//        return id;
-//    }
 
     public String getName() {
         return name;
