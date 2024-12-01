e03aad04bc3e74b8b84540f705e1ef67de99bc0c
davidolleh <asherolleh@gmail.com>
2024-12-02 05:33:38 +0900

    <REFACTOR> 테스트 방식 변경

--- src/test/java/roomescape/JDBCStepTest.java
+++ src/test/java/roomescape/JDBCStepTest.java
@@ -2,16 +2,20 @@ package roomescape;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.test.context.jdbc.Sql;
 import roomescape.api.dto.ReservationResponseDto;
 import roomescape.util.LoggerUtils;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -20,14 +24,24 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Sql(scripts = {"/truncate.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SuppressWarnings("NonAsciiCharacters")
 public class JDBCStepTest {
 
     private static final Logger log = LoggerUtils.logger(JDBCStepTest.class);
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @BeforeAll
+    public static void setup(@Autowired DataSource dataSource) {
+        try (Connection conn = dataSource.getConnection()) {
+
+            ScriptUtils.executeSqlScript(conn, new ClassPathResource("/schema.sql"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     void 오단계() {
@@ -68,13 +82,13 @@ public class JDBCStepTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(201)
-                .header("Location", "/reservations/1");
+                .header("Location", "/reservations/3");
 
         Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
         assertThat(count).isEqualTo(1);
 
         RestAssured.given().log().all()
-                .when().delete("/reservations/1")
+                .when().delete("/reservations/3")
                 .then().log().all()
                 .statusCode(204);
 
@@ -101,7 +115,7 @@ public class JDBCStepTest {
         assertThat(count).isEqualTo(1);
 
         var response = RestAssured.given().log().all()
-                .when().delete("/reservations/3")
+                .when().delete("/reservations/2")
                 .then().log().all().extract().response();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
@@ -111,7 +125,7 @@ public class JDBCStepTest {
     void 특정_예약_테스트() {
         var response = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .when().get("/reservations/1")
+                .when().get("/reservations/5")
                 .then().log().all()
                 .statusCode(200)
                 .extract().response();
