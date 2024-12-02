4d775b555bbf467b4c3c65972ec54d0f9276946d
davidolleh <asherolleh@gmail.com>
2024-12-02 15:31:55 +0900

    <REFACTOR> 로그백 설정 파일 분리

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package roomescape.exception;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
